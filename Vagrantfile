03d33f43356e6fdb2a5aa8571ea6e71b865ac6cc
abr4xas <me@abr4xas.org>
2015-03-28 21:02:00 -0430

    Actualizando repo

--- Vagrantfile
+++ Vagrantfile
@@ -5,35 +5,24 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-
-  config.vm.box = "ubuntu/trusty64"
-  config.ssh.forward_agent = true
-  config.vm.network :forwarded_port, guest: 8000, host: 1234
-  config.vm.boot_timeout = 300
-  config.vm.synced_folder "development/", "/home/vagrant/development"
-
-  config.vm.provision :chef_solo do |chef|
-    chef.cookbooks_path = ["cookbooks"]
-    chef.add_recipe :apt
-    chef.add_recipe 'php'
-    chef.add_recipe 'python'
-    chef.add_recipe 'nodejs'
-    chef.add_recipe 'vim'
-    chef.add_recipe 'git'
-    chef.json = {
-      :git => {
-        :prefix => "/usr/local"
-      }
-    }
-  end
-    
+    config.vm.box = "ubuntu/trusty64"
+    config.ssh.forward_agent = true
+    # Create a private network, which allows host-only access to the machine using a specific IP.
+    config.vm.network "private_network", ip: "192.168.30.10"    
+    config.vm.network :forwarded_port, guest: 80, host: 8080
+    config.vm.network :forwarded_port, guest: 8000, host: 1234   
+    config.vm.boot_timeout = 300
+    config.vm.synced_folder "development/", "/var/www/html", :mount_options => ["dmode=777", "fmode=666"]
     # Shell provisioning
     config.vm.provision "shell" do |s|
         s.path = "provision/setup.sh"
+    end
+    # VirtualBox GUI Name
+    config.vm.provider "virtualbox" do |v|
+        v.name = "Kitchen"
     end
     # VM Name
     # Bringing machine 'Kitchen' up with 'virtualbox' provider...
     config.vm.define :Kitchen do |t|
     end
-
 end
--- provision/setup.sh
+++ provision/setup.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 echo "Provisioning virtual machine..."
-# Git
 echo "Please, wait..."
-apt-get install git -y > /dev/null
-apt-get install npm -y > /dev/null
-apt-get install nodejs -y > /dev/null
-apt-get install python-pip -y > /dev/null
-apt-get install lftp -y > /dev/null
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+# install apache 2.5 and php 5.5
+sudo apt-get install apache2 -y > /dev/null 2>&1
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y > /dev/null 2>&1
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get install phpmyadmin -y > /dev/null 2>&1
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/"
+    <Directory "/var/www/html/">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+# enable mod_rewrite
+sudo a2enmod rewrite
+# restart apache
+service apache2 restart
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+sudo apt-get install git -y > /dev/null 2>&1
+sudo apt-get install nodejs -y > /dev/null 2>&1
+sudo apt-get install npm -y > /dev/null 2>&1
+sudo apt-get install python-pip -y > /dev/null 2>&1
+sudo apt-get install lftp -y > /dev/null 2>&1
 # Install globally dependencies
-npm install -g express
-npm install -g bower
+sudo npm install -g bower
 echo "Finished provisioning."
