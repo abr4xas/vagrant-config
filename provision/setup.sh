b6be675c05195d0fc355701ab0d904443e32779d
ángel <abr4xas@users.noreply.github.com>
2015-06-03 17:48:16 -0430

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 echo "Provisioning virtual machine..."
 echo "Please, wait..."
-# Use single quotes instead of double quotes to make it work with special-character passwords
 PASSWORD='root'
 # update 
 sudo apt-get update -y > /dev/null 2>&1
-# install apache 2.5 and php 5.5
-sudo apt-get install apache2 -y > /dev/null 2>&1
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-cli -y > /dev/null 2>&1
-# install mysql and give password to installer
+sudo apt-get install apache2 mod_rewrite mod_deflate mod_expires mod_headers git python-pip lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl -y > /dev/null 2>&1
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y > /dev/null 2>&1
-# install phpmyadmin and give password(s) to installer
-# for simplicity I'm using the same password for mysql and phpmyadmin
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt-get install phpmyadmin -y > /dev/null 2>&1
-# setup hosts file
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
     DocumentRoot "/var/www/html/"
@@ -32,18 +25,24 @@ VHOST=$(cat <<EOF
 EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
-sudo a2enmod rewrite
-sudo php5enmod mcrypt
+sudo a2enmod rewrite deflate expires headers > /dev/null 2>&1
+sudo php5enmod mcrypt  > /dev/null 2>&1
 sudo rm /etc/php5/apache2/php.ini  > /dev/null 2>&1
-sudo cp php.ini /etc/php5/apache2/ > /dev/null 2>&1
-sudo service apache2 restart
+cd /etc/php5/apache2/  > /dev/null 2>&1
+sudo wget -c https://raw.githubusercontent.com/abr4xas/vagrant-config/master/provision/php.ini > /dev/null 2>&1
+cd > /dev/null 2>&1
+sudo service apache2 restart  > /dev/null 2>&1
 curl -s https://getcomposer.org/installer | php > /dev/null 2>&1
 sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
-sudo apt-get install python-pip -y > /dev/null 2>&1
-sudo apt-get install nodejs -y > /dev/null 2>&1
-sudo apt-get install git -y > /dev/null 2>&1
-sudo apt-get install npm -y > /dev/null 2>&1
-sudo apt-get install lftp -y > /dev/null 2>&1
-# Install globally dependencies
+sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh > /dev/null 2>&1
+sudo curl -sSL https://rvm.io/mpapis.asc | gpg --import - > /dev/null 2>&1
+sudo curl -L https://get.rvm.io | bash -s stable > /dev/null 2>&1
+sudo source ~/.rvm/scripts/rvm > /dev/null 2>&1
+sudo rvm requirements > /dev/null 2>&1
+sudo rvm install ruby > /dev/null 2>&1
+sudo rvm use ruby --default > /dev/null 2>&1
+sudo rvm rubygems current > /dev/null 2>&1
+sudo gem install rails > /dev/null 2>&1
+curl https://npmjs.org/install.sh | sh > /dev/null 2>&1
 sudo npm install -g bower > /dev/null 2>&1
 echo "Finished provisioning."
