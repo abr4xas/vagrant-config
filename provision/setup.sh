dc2432e955e768e09b20ef7eec768d4926cafbba
abr4xas <me@abr4xas.org>
2015-06-24 23:03:40 -0430

    actualizando repo

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,9 +2,14 @@
 echo "Provisioning virtual machine..."
 echo "Please, wait..."
 PASSWORD='root'
-# update 
+# update
 sudo apt-get update -y > /dev/null 2>&1
-sudo apt-get install apache2 mod_rewrite mod_deflate mod_expires mod_headers git python-pip lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl -y > /dev/null 2>&1
+curl -s https://getcomposer.org/installer | php > /dev/null 2>&1
+sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -  > /dev/null 2>&1
+sudo npm install -g bower > /dev/null 2>&1
+sudo apt-get install apache2 mod_rewrite mod_deflate mod_expires mod_headers git python-pip lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl nodejs -y > /dev/null 2>&1
+curl -sL https://npmjs.org/install.sh | sudo sh > /dev/null 2>&1
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y > /dev/null 2>&1
@@ -28,8 +33,4 @@ echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 sudo a2enmod rewrite deflate expires headers > /dev/null 2>&1
 sudo php5enmod mcrypt  > /dev/null 2>&1
 sudo service apache2 restart  > /dev/null 2>&1
-curl -s https://getcomposer.org/installer | php > /dev/null 2>&1
-sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
-curl https://npmjs.org/install.sh | sh > /dev/null 2>&1
-sudo npm install -g bower > /dev/null 2>&1
 echo "Finished provisioning."
