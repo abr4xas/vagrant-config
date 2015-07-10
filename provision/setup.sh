03ec3db3cc69671948841d728f92212cd1aa92e8
abr4xas <me@abr4xas.org>
2015-07-09 20:10:44 -0430

    actualizando setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -5,7 +5,8 @@ PASSWORD='root'
 # update
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
-sudo apt-get install apache2 git lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl python-pip -y /dev/null 2>&1
+echo "Installing few things for the server:..."
+sudo apt-get install apache2 git lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl python-pip -y > /dev/null 2>&1
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y > /dev/null 2>&1
@@ -29,18 +30,22 @@ echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 sudo a2enmod rewrite deflate expires headers > /dev/null 2>&1
 sudo php5enmod mcrypt > /dev/null 2>&1
 sudo service apache2 restart > /dev/null 2>&1
-curl -s https://getcomposer.org/installer | php > /dev/null 2>&1
+echo "Downloading the Composer executable:..."
+curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1
 sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash - > /dev/null 2>&1
+echo "Downloading node & npm:..."
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash - > /dev/null 2>&1
 curl -sL https://npmjs.org/install.sh | sudo sh > /dev/null 2>&1
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list > /dev/null 2>&1
+echo "Adding public key to Rethinkdb:..."
+wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get upgrade -y > /dev/null 2>&1
+echo "Installing node, npm & rethinkdb:..."
+sudo aptitude install nodejs npm rethinkdb -y > /dev/null 2>&1
 sudo npm update -g > /dev/null 2>&1
 sudo npm install nodemon -g > /dev/null 2>&1
 sudo npm install bower -g > /dev/null 2>&1
-source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list > /dev/null 2>&1
-wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add - > /dev/null 2>&1
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get upgrade -y > /dev/null 2>&1
-sudo apt-get install nodejs rethinkdb -y > /dev/null 2>&1
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf > /dev/null 2>&1
 RETHINK=$(cat <<EOF
 runuser=rethinkdb
@@ -53,16 +58,8 @@ EOF
 )
 sudo echo "${RETHINK}" >> /etc/rethinkdb/instances.d/instance1.conf > /dev/null 2>&1
 sudo /etc/init.d/rethinkdb start > /dev/null 2>&1
-LOCALE=$(cat <<EOF
-en_US.UTF-8 UTF-8
-EOF
-)
-sudo echo "${LOCALE}" >> /etc/locale.gen > /dev/null 2>&1
-sudo /usr/sbin/locale-gen > /dev/null 2>&1
-echo "export LANGUAGE=en_US.UTF-8" >> ~/.profile > /dev/null 2>&1
-echo "export LANG=en_US.UTF-8" >> ~/.profile > /dev/null 2>&1
-echo "export LC_ALL=en_US.UTF-8" >> ~/.profile > /dev/null 2>&1
-sudo locale-gen en_US.UTF-8 > /dev/null 2>&1
+sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale > /etc/default/locale > /dev/null 2>&1
 sudo gem install sass -y > /dev/null 2>&1
 sudo apt-get autoremove -y > /dev/null 2>&1
-echo "Finished provisioning."
+sudo update-alternatives --skip-auto > /dev/null 2>&1
+echo "Finished provisioning... Please reboot"
