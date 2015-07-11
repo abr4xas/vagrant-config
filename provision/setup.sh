43dff2562fca20e1bb8c0ec85a545069f114f8b5
abr4xas <me@abr4xas.org>
2015-07-11 11:58:36 -0430

    actualizando setup.sh :smile:

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 echo "Provisioning virtual machine..."
 echo "Please, wait..."
+sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale > /dev/null 2>&1
+sudo echo "LANG=en_US.UTF-8" >> /etc/default/locale > /dev/null 2>&1
+sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale > /dev/null 2>&1
+sudo locale-gen en_US.UTF-8 > /dev/null 2>&1
+sudo dpkg-reconfigure locales > /dev/null 2>&1
 PASSWORD='root'
-# update
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
 echo "Installing few things for the server:..."
 sudo apt-get install apache2 git lftp php5 libapache2-mod-php5 php5-mcrypt php5-cli php5-curl python-pip -y > /dev/null 2>&1
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD" > /dev/null 2>&1
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD" > /dev/null 2>&1
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql -y > /dev/null 2>&1
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true" > /dev/null 2>&1
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD" > /dev/null 2>&1
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD" > /dev/null 2>&1
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD" > /dev/null 2>&1
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" > /dev/null 2>&1
 sudo apt-get install phpmyadmin -y > /dev/null 2>&1
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
@@ -26,7 +30,7 @@ VHOST=$(cat <<EOF
 </VirtualHost>
 EOF
 )
-echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf > /dev/null 2>&1
 sudo a2enmod rewrite deflate expires headers > /dev/null 2>&1
 sudo php5enmod mcrypt > /dev/null 2>&1
 sudo service apache2 restart > /dev/null 2>&1
@@ -35,14 +39,14 @@ curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1
 sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
 echo "Downloading node & npm:..."
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash - > /dev/null 2>&1
-curl -sL https://npmjs.org/install.sh | sudo sh > /dev/null 2>&1
+sudo aptitude install nodejs -y > /dev/null 2>&1
 source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list > /dev/null 2>&1
 echo "Adding public key to Rethinkdb:..."
-wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add - > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
-echo "Installing node, npm & rethinkdb:..."
-sudo aptitude install nodejs npm rethinkdb -y > /dev/null 2>&1
+echo "Installing node & rethinkdb:..."
+sudo aptitude install rethinkdb -y > /dev/null 2>&1
 sudo npm update -g > /dev/null 2>&1
 sudo npm install nodemon -g > /dev/null 2>&1
 sudo npm install bower -g > /dev/null 2>&1
@@ -58,8 +62,7 @@ EOF
 )
 sudo echo "${RETHINK}" >> /etc/rethinkdb/instances.d/instance1.conf > /dev/null 2>&1
 sudo /etc/init.d/rethinkdb start > /dev/null 2>&1
-sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale > /etc/default/locale > /dev/null 2>&1
 sudo gem install sass -y > /dev/null 2>&1
 sudo apt-get autoremove -y > /dev/null 2>&1
-sudo update-alternatives --skip-auto > /dev/null 2>&1
+sudo update-alternatives --all --skip-auto > /dev/null 2>&1
 echo "Finished provisioning... Please reboot"
