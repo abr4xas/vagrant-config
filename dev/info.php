922db3997df846b0108da594621e4cb79804fff7
abr4xas <me@abr4xas.org>
2016-04-18 01:20:49 +0000

    Actualizado info.php
    Se agrega estilos para mejor vista de los datos

--- dev/info.php
+++ dev/info.php
@@ -20,6 +20,8 @@ $mysqli->close();
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js" charset="utf-8"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" charset="utf-8"></script>
   <style type="text/css">
   html, body {
     height: 100%;
@@ -35,6 +37,7 @@ $mysqli->close();
   }
   #footer {
     background-color: #f5f5f5;
+    text-align: center;
   }
   @media (max-width: 767px) {
     #footer {
@@ -43,10 +46,6 @@ $mysqli->close();
       padding-left: 20px;
       padding-right: 20px;
     }
-  }
-  .container {
-    width: auto;
-    max-width: 680px;
   }
   .container .credit {
     margin: 20px 0;
@@ -63,76 +62,121 @@ $mysqli->close();
   .tomato{
     color: tomato
   }
+  h3{
+    text-align: center;
+  }
   </style>
 </head>
 <body>
   <div id="wrap">
     <div class="container">
-      <div class="page-header">
-        <i class="fa fa-lightbulb-o fa-3" aria-hidden="true"></i>
-        <h1>It works!</h1>
+      <div class="row">
+           <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+             <div class="page-header">
+               <i class="fa fa-lightbulb-o fa-3" aria-hidden="true"></i>
+               <h1>It works!</h1>
+             </div>
+             <p class="lead">The Virtual Machine is up and running, yay! Here's some additional information you might need.</p>
+             <div class="row">
+               <div class="col-sm-12">
+                 <div class="panel-group" id="accordion">
+                   <div>
+                     <div class="row">
+                       <div class="col-sm-4">
+                         <div class="">
+                           <div data-toggle="collapse" data-parent="#accordion">
+                             <ul id="tabs" class="nav nav-pills nav-stacked" data-collapse-toggle="collapseOne" data-tabs="tabs">
+                               <li class="active"><a href="#item1" data-toggle="tab">Installed software</a></li>
+                               <li><a href="#item2" data-toggle="tab">PHP Loaded Extensions</a></li>
+                               <li><a href="#item3" data-toggle="tab">MySQL Credentials</a></li>
+                             </ul>
+                           </div>
+                         </div>
+                       </div>
+                       <div class="col-sm-8">
+                         <div id="collapseOne" class="panel panel-default panel-collapse collapse in">
+                           <div class="panel-body">
+                             <div class="tab-content">
+                               <div class="tab-pane fade in active" id="item1">
+                                 <h3>Installed software</h3>
+                                 <div class="table-responsive">
+                                   <table class="table table-striped">
+                                     <tr>
+                                       <td>PHP Version</td>
+                                       <td><?php echo phpversion(); ?></td>
+                                     </tr>
+                                     <tr>
+                                       <td>MySQL running</td>
+                                       <td>
+                                         <?php
+                                           echo ($mysql_running ? '¡Hell Yeah!' : 'Oh balls!!');
+                                         ?>
+                                         <i class="fa <?php echo ($mysql_running ? 'fa-check tomato' : 'fa-times'); ?> " aria-hidden="true"></i></td>
+                                     </tr>
+                                     <tr>
+                                       <td>MySQL version</td>
+                                       <td><?php echo ($mysql_running ? $mysql_version : 'N/A'); ?></td>
+                                     </tr>
+                                     <tr>
+                                       <td>NGINX</td>
+                                       <td><?php echo $_SERVER['SERVER_SOFTWARE']; ?></td>
+                                     </tr>
+                                   </table>
+                                 </div>
+                               </div>
+                               <div class="tab-pane fade" id="item2">
+                                 <h3>PHP Loaded Extensions</h3>
+                                 <div class="table-responsive">
+                                 <table class="table table-striped">
+                                   <?php
+                                   $extdir   = ini_get('extension_dir');
+                                   $modules  = get_loaded_extensions();
+                                   foreach($modules as $m){
+                                         $lib  = $extdir.'/'.$m.'.so';
+                                   echo '
+                                   <tr>
+                                     <td>'.$m.'</td>'; ?>
+                                       <td><?php echo (file_exists($lib) ? '¡Hell Yeah!' : 'Oh balls!!' ); ?> <i class="fa <?php echo (file_exists($lib) ? 'fa-check tomato' : 'fa-times' ); ?>" aria-hidden="true"></i></td>
+                                   <?php
+                                   echo '</tr>';
+                                   }
+                                   ?>
+                                 </table>
+                               </div>
+                             </div>
+                               <div class="tab-pane fade" id="item3">
+                                 <h3>MySQL Credentials</h3>
+                                 <div class="table-responsive">
+                                 <table class="table table-striped">
+                                   <tr>
+                                     <td>Hostname</td>
+                                     <td>localhost</td>
+                                   </tr>
+                                   <tr>
+                                     <td>Username</td>
+                                     <td>root</td>
+                                   </tr>
+                                   <tr>
+                                     <td>Password</td>
+                                     <td>root</td>
+                                   </tr>
+                                   <tr>
+                                     <td colspan="2"><em>Note: External access is enabled! Just use <strong><?php echo $_SERVER['SERVER_ADDR'] ?></strong> as host.</em></td>
+                                   </tr>
+                                 </table>
+                               </div>
+                             </div>
+                             </div><!-- / my-tab-content -->
+                           </div><!-- / panel-body -->
+                         </div><!-- / panel-collapse collapse in -->
+                       </div><!-- / col-lg-8 -->
+                     </div><!-- / row -->
+                   </div><!-- / panel panel-default -->
+                 </div><!-- / accordion -->
+               </div><!-- / col-lg-12 -->
+             </div>
+           </div>
       </div>
-      <p class="lead">The Virtual Machine is up and running, yay! Here's some additional information you might need.</p>
-
-      <h3>Installed software</h3>
-      <table class="table table-striped">
-        <tr>
-          <td>PHP Version</td>
-          <td><?php echo phpversion(); ?></td>
-        </tr>
-        <tr>
-          <td>MySQL running</td>
-          <td>
-
-            <?php
-          echo   ($mysql_running ? '¡Hell Yeah!' : 'Oh balls!!');
-            ?>
-            <i class="fa <?php echo ($mysql_running ? 'fa-check tomato' : 'fa-times'); ?> " aria-hidden="true"></i></td>
-        </tr>
-        <tr>
-          <td>MySQL version</td>
-          <td><?php echo ($mysql_running ? $mysql_version : 'N/A'); ?></td>
-        </tr>
-      </table>
-
-      <h3>PHP Modules</h3>
-      <table class="table table-striped">
-        <?php
-        $extdir=ini_get('extension_dir');
-        $modules=get_loaded_extensions();
-        foreach($modules as $m){
-              $lib=$extdir.'/'.$m.'.so';
-        echo '
-        <tr>
-          <td>'.$m.'</td>'; ?>
-            <td><?php echo (file_exists($lib) ? '¡Hell Yeah!' : 'Oh balls!!' ); ?> <i class="fa <?php echo (file_exists($lib) ? 'fa-check tomato' : 'fa-times' ); ?>" aria-hidden="true"></i></td>
-        <?php
-        echo '</tr>';
-        }
-        ?>
-      </table>
-
-      <h3>MySQL credentials</h3>
-      <table class="table table-striped">
-        <tr>
-          <td>Hostname</td>
-          <td>localhost</td>
-        </tr>
-
-        <tr>
-          <td>Username</td>
-          <td>root</td>
-        </tr>
-
-        <tr>
-          <td>Password</td>
-          <td>root</td>
-        </tr>
-
-        <tr>
-          <td colspan="2"><em>Note: External access is enabled! Just use <strong><?php echo $_SERVER['SERVER_ADDR'] ?></strong> as host.</em></td>
-        </tr>
-      </table>
     </div>
 
     <div id="push"></div>
